132615db9f54aa197e92a6fd612cb5fa4b35f1be
Shwetha Kandhalu <159231606+shwe-kandhalu@users.noreply.github.com>
2024-10-25 18:08:49 +0000

    progress

---
+++ clean_data.py
@@ -0,0 +1,25 @@
+# clean_data.py
+
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('messy_population_data.csv')
+
+df.info()
+
+df.describe(include='all')
+
+df.isnull().sum()
+
+df.duplicated().sum()
+
+print(df.dtypes)
+
+unique_counts = df.nunique()
+print(unique_counts)
+
+"""
+for col in df.columns:
+    print(f"Value counts for {col}:")
+    print(df[col].value_counts())
+"""
